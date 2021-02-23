da1644a10c7e7cd412d5d8a3a5cded948f8013fb
himitery <himit0131@naver.com>
2021-02-24 04:47:02 +0900

    SW Expert Academy: Tree 문제 리팩토링 - 5176: 이진탐색

--- SwExpertAcademy/Programming Intermediate/Tree/5176.py
+++ SwExpertAcademy/Programming Intermediate/Tree/5176.py
@@ -2,80 +2,27 @@ case = int(input())
 
 
 class BinaryTree:
-    class Node:
-        def __init__(self, data):
-            self.data = data
-            self.left = None
-            self.right = None
+    def __init__(self, n):
+        self.n = n
+        self.count = 1
+        self.sequence = [0] * (n + 1)
+        self.setSequence(1)
 
-    def __init__(self, num):
-        self.numList = [x + 1 for x in range(num)]
-        self.sequence = list()
-        self.main()
+    def setSequence(self, num):
+        if num <= self.n:
+            self.setSequence(num * 2)
+            self.sequence[num] = self.count
+            self.count += 1
+            self.setSequence(num * 2 + 1)
 
-    def main(self):
-        self.setSequence(self.numList, 0)
-
-        sequence = list()
-        for layerList in self.sequence:
-            for data in layerList:
-                sequence.append(data)
-        self.sequence = sequence
-        self.sequence += sorted(
-            list(set(self.numList) - (set(self.numList) & set(self.sequence)))
-        )
-
-    def getRootNode(self, length, start=None):
-        layer = 1
-        while True:
-            if 2 ** layer - 1 <= length and 2 ** (layer + 1) - 1 > length:
-                layer += 1
-                break
-            layer += 1
-
-        return self.Node(
-            (
-                (length - (2 ** (layer - 1) - 1))
-                if 2 ** (layer - 1) // 2 > (length - (2 ** (layer - 1) - 1))
-                else (2 ** (layer - 1) // 2)
-            )
-            + (2 ** (layer - 1) - 2) // 2
-            + 1
-            + (start - 1 if start is not None else 0)
-        )
-
-    def setSequence(self, numList, layer):
-        rootNode = self.getRootNode(len(numList), numList[0])
-        rootNodeIndex = numList.index(rootNode.data)
-        self.sequence[layer].append(rootNode.data) if len(
-            self.sequence
-        ) > layer else self.sequence.append([rootNode.data])
-
-        leftSubTree = numList[:rootNodeIndex]
-        rightSubTree = numList[rootNodeIndex + 1 :]
-
-        if len(numList) > 3:
-            self.setSequence(leftSubTree, layer + 1)
-            self.setSequence(rightSubTree, layer + 1)
-        elif len(numList) == 3:
-            self.sequence[layer + 1].append(leftSubTree[0]) if len(
-                self.sequence
-            ) > layer + 1 else self.sequence.append([leftSubTree[0]])
-            self.sequence[layer + 1].append(rightSubTree[0]) if len(
-                self.sequence
-            ) > layer + 1 else self.sequence.append([rightSubTree[0]])
-
-    def getSequence(self):
-        return self.sequence
+    def getResult(self):
+        return [self.sequence[1], self.sequence[self.n // 2]]
 
 
 for i in range(case):
     n = int(input())
 
     tree = BinaryTree(n)
-    sequence = tree.getSequence()
+    result = tree.getResult()
 
-    rootNode = sequence[0]
-    specificNode = sequence[n // 2 - 1]
-
-    print(f"#{i+1} {rootNode} {specificNode}")
+    print(f"#{i+1} {result[0]} {result[1]}")
