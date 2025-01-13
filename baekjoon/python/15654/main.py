ecd44cea549f0469ffee7da25e0d77fa3e6aa6b1
himitery <himitery@gmail.com>
2025-01-13 16:48:10 +0900

    solved(python): baekjoon 15654

---
+++ baekjoon/python/15654/main.py
@@ -0,0 +1,19 @@
+import sys
+
+from itertools import permutations
+
+read = lambda: sys.stdin.readline().rstrip()
+
+
+class Problem:
+    def __init__(self):
+        self.n, self.m = map(int, read().split())
+        self.data = list(map(int, read().split()))
+
+    def solve(self) -> None:
+        for perm in sorted(permutations(self.data, self.m)):
+            print(" ".join(map(str, perm)))
+
+
+if __name__ == "__main__":
+    Problem().solve()
