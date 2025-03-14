e10b0b71b013a24af3cf81bc079f9dc91b1db7f0
himitery <himitery@gmail.com>
2025-03-14 18:16:42 +0900

    solved(python): programmers
    - PCCP 기출문제 / 퍼즐 게임 챌린지

---
+++ programmers/PCCP_기출문제/python/퍼즐_게임_챌린지/main.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def solution(diffs: List[int], times: List[int], limit: int) -> int:
+    result, left, right = 0, 1, max(diffs)
+
+    while left <= right:
+        level = (left + right) // 2
+        if level == result:
+            break
+
+        total = 0
+        for idx, diff in enumerate(diffs):
+            total += times[idx] + ((times[idx - 1] + times[idx]) * (diff - level) if level < diff else 0)
+            if total > limit:
+                break
+
+        if total <= limit:
+            result, right = level, level
+        else:
+            left = level + 1
+
+    return max(1, result)
+
+
+if __name__ == "__main__":
+    solution(
+        list(map(int, input().split())),
+        list(map(int, input().split())),
+        int(input()),
+    )
